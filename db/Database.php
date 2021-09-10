b9d8d727179aacd3431b056383593cd2f81d1718
Lacheheb Thomas <thomas.lacheheb@hotmail.fr>
2021-09-10 13:58:51 +0200

    Add local variable for database

--- db/Database.php
+++ db/Database.php
@@ -3,13 +3,15 @@
 class Database
 {
     const HOSTNAME = 'db';
+    const HOSTNAME_LOCAL = '127.0.0.1';
     private $db = 'blog';
     const USERNAME = 'root';
     const PASSWORD = '';
 
     public function connexion(){
         try{
-            return new PDO('mysql:host='.self::HOSTNAME.';dbname='.$this->db, self::USERNAME, self::PASSWORD);
+            // return new PDO('mysql:host='.self::HOSTNAME.';dbname='.$this->db, self::USERNAME, self::PASSWORD);
+            return new PDO('mysql:host='.self::HOSTNAME_LOCAL.';dbname='.$this->db, self::USERNAME, self::PASSWORD);
         }
         catch(Exception $e){
             die('Erreur : '. $e->getMessage());
